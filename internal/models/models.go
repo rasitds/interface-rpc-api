3c1b788d9bd961400b0552279016135542d21e61
durmusrasit <mrdurmus@outlook.com>
2023-01-01 20:18:48 +0300

    Add AutoMigrate function

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,6 +1,15 @@
 package models
 
+import "gorm.io/gorm"
+
 type Theme struct {
 	Background string `json:"background"`
 	Foreground string `json:"foreground"`
+}
+
+func AutoMigrate(db *gorm.DB) error {
+	if err := db.AutoMigrate(&Theme{}); err != nil {
+		return err
+	}
+	return nil
 }
